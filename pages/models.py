25ad6a7e368e4f95cdb61ded7310c1666704d083
Vannger <kirillpickaxe2010@list.ru>
2026-06-12 01:12:51 +0300

    Task 5 ready

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Spell(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Заклинание')
+    spell_lvl = models.IntegerField(verbose_name='Уровень')
+    desc = models.TextField(verbose_name='Описание')
+    add_date = models.DateField(verbose_name='Дата добавления')
+    def __str__(self):
+        return f'{self.name}, Уровень {self.spell_lvl}'
