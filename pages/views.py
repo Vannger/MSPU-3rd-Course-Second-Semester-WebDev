b140372ff8f62db3ee665f6272d8433f34816bb5
Vannger <kirillpickaxe2010@list.ru>
2026-06-12 00:27:53 +0300

    Task 4 ready

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {
+        'title': 'The definitive DnD collection of spells',
+        'welcome_text': 'Hello and Welcome, ladies and gentleman, to, well, THIS'
+    }
+    return render(request, 'pages/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'The definitive DnD collection of spells',
+        'text': 'Text that i would like'
+    }
+    return render(request, 'pages/about.html', context)
